2839f5f5e2b6042a35e33162481395ec97e51056
Larissa Soares <larissa.soares@neugelb.com>
2024-08-24 17:27:48 +0200

    feat: add welcome screen

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
-class Colors {
-  Colors._();
+class AppColors {
+  AppColors._();
   static const Color primary = Color(0xFF322E53);
   static const Color secondary = Color(0xFFF3AB9D);
   static const Color white = Color(0xFFFFFFFF);
