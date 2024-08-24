7e632945be4f21689167447586b572a78f8f84fb
Larissa Soares <larissa.soares@neugelb.com>
2024-08-24 15:44:44 +0200

    first commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class Colors {
+  Colors._();
+  static const Color primary = Color(0xFF322E53);
+  static const Color secondary = Color(0xFFF3AB9D);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF0A0304);
+  static const Color divider = Color(0xFFE4E4E4);
+  static const Color background = Color(0x17322E53);
+  static const Color brightShade = Color(0xFFE7E7E7);
+}
