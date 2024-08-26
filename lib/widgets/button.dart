53a9d62bda0894294bd9a64f03904c46916da87b
Beatriz Nonato <bia_nonato@icloud.com>
2024-08-26 16:57:23 -0300

    feat: add login screen and links navigation

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:mobiefy_flutter/constants/colors.dart';
 import 'package:mobiefy_flutter/constants/fonts.dart';
 
-class Button extends StatelessWidget {
+class CustomButton extends StatelessWidget {
   final String label;
   final VoidCallback onPressed;
   final Color color;
   final Color textColor;
 
-  const Button({
+  const CustomButton({
     super.key,
     required this.label,
     required this.onPressed,
-    this.color = AppColors.secondary,
-    this.textColor = AppColors.primary,
+    this.color = AppColors.primary,
+    this.textColor = AppColors.white,
   });
 
   @override
@@ -23,7 +23,6 @@ class Button extends StatelessWidget {
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           backgroundColor: color,
-          foregroundColor: textColor,
           padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(30.0),
@@ -32,7 +31,8 @@ class Button extends StatelessWidget {
         onPressed: onPressed,
         child: Text(
           label,
-          style: AppFonts.text.copyWith(fontWeight: FontWeight.w700),
+          style: AppFonts.text
+              .copyWith(fontWeight: FontWeight.w700, color: textColor),
         ),
       ),
     );
