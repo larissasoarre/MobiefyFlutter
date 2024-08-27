689c04268fd34dc07a58f87e0a6e45d3dd7c0640
Larissa Soares <larissa.soares@neugelb.com>
2024-08-27 08:06:01 +0200

    fix: update form fields on screens

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -4,27 +4,42 @@ import 'package:mobiefy_flutter/constants/fonts.dart';
 import 'package:mobiefy_flutter/views/signup_screen.dart';
 import 'package:mobiefy_flutter/widgets/button.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: PageContent(),
+        body: SingleChildScrollView(child: PageContent()),
       ),
     );
   }
 }
 
-class PageContent extends StatelessWidget {
+class PageContent extends StatefulWidget {
   const PageContent({super.key});
 
   @override
+  State<PageContent> createState() => _PageContentState();
+}
+
+class _PageContentState extends State<PageContent> {
+  bool _hidePassword = true;
+
+  @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Container(
       padding: const EdgeInsets.fromLTRB(38.0, 60.0, 38.0, 25.0),
       color: AppColors.white,
+      height: screenHeight,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
@@ -42,43 +57,57 @@ class PageContent extends StatelessWidget {
                 .copyWith(color: AppColors.primary, fontSize: 32),
           ),
           const SizedBox(height: 17.0),
-          Column(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("E-mail", style: AppFonts.inputLabel),
-                  TextField(
-                    decoration: InputDecoration(
+          Form(
+            child: Column(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("E-mail", style: AppFonts.inputLabel),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: AppColors.brightShade,
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(15))),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 30.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Senha", style: AppFonts.inputLabel),
+                    TextFormField(
+                      obscureText: _hidePassword,
+                      decoration: InputDecoration(
                         filled: true,
                         fillColor: AppColors.brightShade,
                         border: OutlineInputBorder(
                             borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.circular(15))),
-                  )
-                ],
-              )
-            ],
-          ),
-          const SizedBox(height: 30.0),
-          Column(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("Senha", style: AppFonts.inputLabel),
-                  TextField(
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: AppColors.brightShade,
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.circular(20))),
-                  ),
-                  const SizedBox(height: 30.0)
-                ],
-              )
-            ],
+                            borderRadius: BorderRadius.circular(20)),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _hidePassword
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: AppColors.primary,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _hidePassword =
+                                  !_hidePassword; // Toggle visibility
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30.0)
+                  ],
+                )
+              ],
+            ),
           ),
           Column(
             children: [
