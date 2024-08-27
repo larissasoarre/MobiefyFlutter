bd028bd0047626b9bdd16110a1873772e4da248c
Larissa Soares <larissa.soares@neugelb.com>
2024-08-28 01:25:42 +0200

    feat: add google maps to home

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,21 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mobiefy_flutter/constants/colors.dart';
-import 'package:mobiefy_flutter/constants/fonts.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-    return Container(
-      height: screenHeight,
-      padding: const EdgeInsets.fromLTRB(38.0, 60.0, 38.0, 25.0),
-      color: AppColors.white,
-      child: const Text(
-        'Home Screen',
-        style: AppFonts.heading,
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            // Google Map
+            const Map(), // This will be the background widget
+
+            DraggableScrollableSheet(
+              initialChildSize: 0.2,
+              maxChildSize: 0.3,
+              minChildSize: 0.2,
+              builder: (BuildContext context, scrollController) {
+                return Container(
+                  clipBehavior: Clip.hardEdge,
+                  decoration: const BoxDecoration(
+                    color: AppColors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
+                      topRight: Radius.circular(25),
+                    ),
+                  ),
+                  child: CustomScrollView(
+                    controller: scrollController,
+                    slivers: [
+                      SliverToBoxAdapter(
+                        child: Center(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).hintColor,
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(10)),
+                            ),
+                            height: 4,
+                            width: 40,
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                          ),
+                        ),
+                      ),
+                      SliverList.list(children: const [
+                        ListTile(
+                            title: SizedBox(
+                          height: 20.0,
+                        )),
+                        ListTile(
+                            title: Text(
+                          'Mobiefy em desenvolvimento!',
+                          textAlign: TextAlign.center,
+                        )),
+                      ])
+                    ],
+                  ),
+                );
+              },
+            )
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class Map extends StatefulWidget {
+  const Map({super.key});
+
+  @override
+  State<Map> createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  static const testLocation = LatLng(37.4223, -122.0848);
+  @override
+  Widget build(BuildContext context) {
+    return const GoogleMap(
+      initialCameraPosition: CameraPosition(target: testLocation, zoom: 13),
     );
   }
 }
